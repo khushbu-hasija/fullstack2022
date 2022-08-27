69322f63451fd3441a5ebb266295aae63962a077
Khushbu Hasija <khushbuhasija@Khushbus-MacBook-Air.local>
2022-08-27 18:56:30 +0530

    Added submission for 1.1 and 1.2

---
+++ part1/step1/src/components/Content.js
@@ -0,0 +1,12 @@
+import Part from "./Part";
+const Content = (props) => {
+    return (
+    <>
+        <Part part={props.parts[0]} exercise={props.exercises[0]}/>  
+        <Part part={props.parts[1]} exercise={props.exercises[1]}/>
+        <Part part={props.parts[2]} exercise={props.exercises[2]}/>
+    </>
+    )
+}
+
+export default Content;
