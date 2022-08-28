abeaf2dc942291762698ee44d6a3b34a7abf1a11
Khushbu Hasija <khushbuhasija@Khushbus-MacBook-Air.local>
2022-08-28 22:33:14 +0530

    Done 1.12

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -1,42 +1,25 @@
-import { useState } from 'react'
 import React from 'react'
-import Statistics from './components/Statistics'
+import { useState } from 'react'
 
 const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-  const [avg,setAvg] = useState(0)
-  const [showStats, setShowStats] = useState(false)
-
-  const setToGood = (val) => {
-    setShowStats(true)
-    setGood(val)
-    setAvg(avg+1)
-  }
-
-  const setToBad = (val) => {
-    setShowStats(true)
-    setBad(val)
-    setAvg(avg-1)
-  }
-
-  const setToNeutral = (val) => {
-    setShowStats(true)
-    setNeutral(val)
-  }
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.'
+  ]
+  const [random, setRandom] = useState(0)
+  const setRandomFn = () => setRandom(Math.floor(Math.random() * anecdotes.length));
 
   return (
     <div>
-      <h1>give feedback</h1>
-      <button onClick={() => setToGood(good+1)}>good</button>
-      <button onClick={() => setToNeutral(neutral+1)}>neutral</button>
-      <button onClick={() => setToBad(bad+1)}>bad</button>
-      { showStats
-       ? <Statistics good={good} bad={bad} neutral={neutral} avg={avg}/>
-       : <p>No feedback given</p> }
-      </div>
+      {anecdotes[random]}
+      <br/>
+      <button onClick={() => setRandomFn()}>next anecdote</button>
+    </div>
   )
 }
 
